d6ce48018cedfc58e7f1cf0129e33a23aa2a6958
Grancan91 <abl.lopper@gmail.com>
2024-09-27 10:29:43 +0100

    Insert Player

--- main.js
+++ main.js
@@ -3,4 +3,6 @@ let playField = document.getElementById('playfield')
 
 let player = new Player(275, 650)
 
+player.insert()
+
 console.log(player)
--- player.js
+++ player.js
@@ -8,4 +8,15 @@ class Player{
     this.speed = 10
     this.sprite = document.createElement('div')
   }
+
+  insert(){
+    this.sprite.setAttribute('id', 'player')
+    this.sprite.style.width = this.width  + 'px'
+    this.sprite.style.height = this.height  + 'px'
+    this.sprite.style.top = this.y + 'px'
+    this.sprite.style.left = this.x  + 'px'
+    playField.appendChild(this.sprite)
+  }
+
 }
+
